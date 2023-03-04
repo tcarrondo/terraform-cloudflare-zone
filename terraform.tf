b94d598e4db457785a30267a71d48105a79b82ae
Tiago Carrondo <tiago@carrondo.pt>
2023-03-04 22:40:13 +0000

    bump CF provider version

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
+      version = ">= 3.32"
     }
   }
-  required_version = "~> 1.0"
+  required_version = ">= 1.0"
 }
