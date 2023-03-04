0553a82a36ead5a17b35b648731c97241e656b23
Tiago Carrondo <tiago@carrondo.pt>
2023-03-04 22:41:51 +0000

    add output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "domain" {
-  value = var.domain
+  description = "Zone domain name"
+  value       = var.domain
 }
 
 output "ns" {
-  value = cloudflare_zone.domain.0.name_servers
+  description = "Zone nameservers"
+  value       = cloudflare_zone.domain.0.name_servers
 }
 
 output "zone_id" {
-  value = cloudflare_zone.domain.0.id
+  description = "Zone ID"
+  value       = cloudflare_zone.domain.0.id
 }
